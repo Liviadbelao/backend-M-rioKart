7d87ea50c407c9053cff2cebefbb62f907b8e8cc
Giulia Marques <giuliamarques.564@gmail.com>
2023-11-27 15:04:49 -0300

    feat: membros fixos

--- src/controllers/usuario.contollers.js
+++ src/controllers/usuario.contollers.js
@@ -1,8 +1,14 @@
 import { Usuario } from '../models/Usuarios/Usuarios.js'
 import { usuarioList } from '../models/usuarios/usuariosList.js'
+import membros from '../data/membros.js';
 
 
 const lista = new usuarioList();
+
+membros.map((membro) => {
+    const novoMembro = new Usuario(membro.nome, membro.idade, membro.tipo, membro.descricao)
+    lista.addUsuarios(novoMembro)
+})
 
 export const pegarUsuariosId = (req, res) => {
     const { id } = req.params;
@@ -13,91 +19,91 @@ export const pegarUsuariosId = (req, res) => {
 
 
 export const pegarTodos = (req, res) => {
-const {nome, tipo,idade}=req.query;
+    const { nome, tipo, idade } = req.query;
     let usuarios = lista.pegarTodos();
-if(nome){
-    usuarios=lista.pegarPeloNome(nome)
-}
-if(tipo){
-    usuarios=lista.pegarPeloTipo(tipo)
-}
-if(idade){
-    usuarios=lista.pegarPeloIdade(Number(idade))
-}
+    if (nome) {
+        usuarios = lista.pegarPeloNome(nome)
+    }
+    if (tipo) {
+        usuarios = lista.pegarPeloTipo(tipo)
+    }
+    if (idade) {
+        usuarios = lista.pegarPeloIdade(Number(idade))
+    }
 
-if(usuarios.length<0){
-    return res.status(400).json({message: "USuario não enco"})
+    if (usuarios.length < 0) {
+        return res.status(400).json({ message: "USuario não enco" })
 
-   
-}else{
-    let contador= usuarios.length
-    return res.status(200).send({contador, usuarios})
-}
+
+    } else {
+        let contador = usuarios.length
+        return res.status(200).send({ contador, usuarios })
+    }
 }
 
 export const addUsuarios = ((req, res) => {
     const { nome, idade, imagem, descricao, tipo } = req.body
-    let error=[];
+    let error = [];
 
-    if(nome.length > 40){
+    if (nome.length > 40) {
         error.push("o nome deve ter menos que 40 caracteres")
     }
-    if(descricao.length > 40){
+    if (descricao.length > 40) {
         error.push("o descricao deve ter menos que 40 caracteres")
     }
 
-    if(idade < 13){
+    if (idade < 13) {
         error.push("o usuario deve ser maior de 13 anos")
     }
 
-    if(!imagem.match(/(https?:\/\/.*.(?:png|jpg|jpeg))/i)){
+/*     if (!imagem.match(/(https?:\/\/.*.(?:png|jpg|jpeg))/i)) {
         error.push("A imagem deve um URL válido que termine em png, jpg ou jpeg.")
-      }
+    } */
 
-  if(error.length > 0){
-       return res.status(400).send({message: error})
-    }else{
+    if (error.length > 0) {
+        return res.status(400).send({ message: error })
+    } else {
         const usuario = new Usuario(nome, idade, imagem, descricao, tipo)
         lista.addUsuarios(usuario);
         return res.status(201).send({ message: "usuariooo criooooouuu", usuario })
     }
-   
+
 })
 
 export const editarUsuario = ((req, res) => {
     const { id } = req.params;
     const { nome, idade, imagem, descricao, tipo } = req.body;
 
-    let error=[];
+    let error = [];
 
-    if(nome.length > 40){
+    if (nome.length > 40) {
         error.push("o nome deve ter menos que 40 caracteres")
     }
-    if(descricao.length > 40){
+    if (descricao.length > 40) {
         error.push("o descricao deve ter menos que 40 caracteres")
     }
 
-    if(idade.length < 13){
+    if (idade.length < 13) {
         error.push("o usuario deve ser maior de 13 anos")
     }
 
 
- 
-    if(!imagem.match(/(https?:\/\/.*.(?:png|jpg|jpeg))/i)){
-        error.push("A imagem deve um URL válido que termine em png, jpg ou jpeg.")
-      }
 
-  if(error.length){
-       return res.status(400).send({message: error})
-    }else{
+    if (!imagem.match(/(https?:\/\/.*.(?:png|jpg|jpeg))/i)) {
+        error.push("A imagem deve um URL válido que termine em png, jpg ou jpeg.")
+    }
+
+    if (error.length) {
+        return res.status(400).send({ message: error })
+    } else {
         const usuario = new Usuario(id, nome, idade, imagem, descricao, tipo)
         lista.editarUsuario(usuario);
         return res.status(200).send({ message: `Atualizou boy`, usuario })
 
     }
-   
 
-   
+
+
 
 })
 
---
+++ src/data/membros.js
@@ -0,0 +1,40 @@
+const membros = [
+    {
+        nome: "Giulia Marques",
+        idade: "17",
+        tipo: "Alun(o)a",
+        descricao: "Aluna do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    },
+    { 
+        nome: "Manuela Liberato",
+        idade: "16",
+        tipo: "Alun(o)a",
+        descricao: "Aluna do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    },
+    {
+        nome: "Nickolas Mamede",
+        idade: "17",
+        tipo: "Alun(o)a",
+        descricao: "Aluno do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    },
+    {
+        nome: "Maria Cancian",
+        idade: "17",
+        tipo: "Alun(o)a",
+        descricao: "Aluna do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    },
+    {
+        nome: "Maria Rita Gomes",
+        idade: "16",
+        tipo: "Alun(o)a",
+        descricao: "Aluna do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    },
+    {
+        nome: "Lívia Belão",
+        idade: "17",
+        tipo: "Alun(o)a",
+        descricao: "Aluna do SESI/SENAI - Valinhos. Cursando Desenvolvimento de Sistema, segundo semstre."
+    }
+
+]
+export default membros;
